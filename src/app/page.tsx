16bc044fad5826a3fe18162ffe0c044ea1a7c74d
Nicholas Victoros <nvictoros21@gmail.com>
2024-12-16 00:48:24 +0000

    Fix lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,9 @@ export default async function Home() {
       <div>{session?.user?.name}</div>
       <Image alt="" width={100} height={100} src={session?.user?.image || ''} />
       {topTracks && (<ul>
-        {topTracks.items.map(({ name, artists }: any, index: number) => <li>{index + 1}. {name} | {artists.reduce((artists: string, artist: { name: string }, i: number) => artists += (i ? ', ' : '') + artist.name, '')}</li>)}
+        {topTracks.items.map(({ name, artists, id }: { name: string, artists: { name: string }[], id: string }, index: number) =>
+          <li key={id}>{index + 1}. {name} | {artists.reduce((artists: string, artist, i: number) => artists += (i ? ', ' : '') + artist.name, '')}</li>
+        )}
       </ul>)}
       <button onClick={async () => { 'use server'; await signOut({ redirectTo: '/login' }) }}>Sign Out</button>
     </SessionProvider >
