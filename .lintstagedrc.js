14b52837dd1008d48bb5f8698d74fab763c7ab67
Nicholas Victoros <nvictoros21@gmail.com>
2024-12-27 19:28:03 +0000

    Add dynamic height

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,6 +4,6 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx,css}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
   '*.{js,jsx,ts,tsx,json,css}': ['prettier --write .'],
 };
