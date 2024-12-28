2c200eb1abc8e734d0ec1fb2735dd487101268c2
Nicholas Victoros <nvictoros21@gmail.com>
2024-12-28 03:45:40 +0000

    Convert menu into modal

--- src/app/ui/header/Header.tsx
+++ src/app/ui/header/Header.tsx
@@ -4,79 +4,50 @@ import Image from 'next/image';
 import styles from './Header.module.css';
 import { useSession } from 'next-auth/react';
 import { useEffect, useRef, useState } from 'react';
-import { Themes } from '../types/theme.types';
+import { createPortal } from 'react-dom';
+import { Menu } from '../menu/Menu';
 
 type HeaderProps = {
   onSignOutClick: () => void;
 };
 
 export const Header = ({ onSignOutClick }: HeaderProps) => {
-  const [showSubMenu, setShowSubMenu] = useState(false);
-  const subMenuRef = useRef<HTMLDivElement>(null);
+  const [showMenu, setShowMenu] = useState(false);
   const { data: session } = useSession();
+  const menuRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const handleDocClick = (event: MouseEvent) => {
       if (event.target instanceof Element) {
-        if (!subMenuRef.current?.contains(event.target)) {
-          if (showSubMenu) setShowSubMenu(false);
+        if (!menuRef.current?.contains(event.target)) {
+          if (showMenu) {
+            setShowMenu(false);
+            event.stopPropagation();
+          }
         }
       }
     };
 
-    document.body.addEventListener('pointerdown', handleDocClick);
+    document.body.addEventListener('click', handleDocClick);
 
     return () => {
-      document.body.removeEventListener('pointerdown', handleDocClick);
+      document.body.removeEventListener('click', handleDocClick);
     };
-  }, [showSubMenu]);
+  }, [showMenu]);
 
   return (
     <header className={styles.header}>
       <h1 className={styles.heading}>TopSpot</h1>
-      <div>
-        <Image
-          onClick={() => setShowSubMenu((show) => !show)}
-          className={styles.avatar}
-          alt=""
-          width={50}
-          height={50}
-          src={session?.user?.image || ''}
-        />
-      </div>
-      <div ref={subMenuRef} className={showSubMenu ? styles.subMenu : `${styles.subMenu} ${styles.hiddenSubMenu}`}>
-        <div className={styles.subMenuProfile}>
-          <Image
-            onClick={() => setShowSubMenu((show) => !show)}
-            className={styles.avatar}
-            alt=""
-            width={50}
-            height={50}
-            src={session?.user?.image || ''}
-          />
-          <div>
-            <p className={styles.name}>{session?.user?.name}</p>
-            <p className={styles.email}>{session?.user?.email}</p>
-          </div>
-        </div>
-        <div className={styles.subMenuItem}>
-          Theme:{' '}
-          <select
-            onChange={({ target }) => {
-              document.documentElement.className = target.value;
-              window.localStorage.setItem('theme', target.value);
-            }}
-            defaultValue={window.localStorage.getItem('theme') || Themes.OSDefault}
-          >
-            <option value={Themes.OSDefault}>OS default</option>
-            <option value={Themes.Light}>Light</option>
-            <option value={Themes.Dark}>Dark</option>
-          </select>
-        </div>
-        <button className={styles.subMenuItem} onClick={onSignOutClick}>
-          Log Out
-        </button>
-      </div>
+      <Image
+        onClick={() => setShowMenu((show) => !show)}
+        role="button"
+        className={styles.avatar}
+        alt=""
+        width={50}
+        height={50}
+        src={session?.user?.image || ''}
+      />
+      {showMenu && createPortal(<Menu onSignOutClick={onSignOutClick} ref={menuRef} />, document.body)}
     </header>
   );
 };
---
+++ src/app/ui/menu/Menu.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import styles from './Menu.module.css';
+import { Themes } from '../types/theme.types';
+import { useSession } from 'next-auth/react';
+
+type MenuProps = {
+  ref?: React.RefObject<HTMLDivElement | null>;
+  onSignOutClick: () => void;
+};
+
+export const Menu = ({ ref, onSignOutClick }: MenuProps) => {
+  const { data: session } = useSession();
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.menu} ref={ref}>
+        <div className={styles.menuProfile}>
+          <Image className={styles.avatar} alt="" width={50} height={50} src={session?.user?.image || ''} />
+          <div>
+            <p className={styles.name}>{session?.user?.name}</p>
+            <p className={styles.email}>{session?.user?.email}</p>
+          </div>
+        </div>
+        <div className={styles.menuItem}>
+          Theme:{' '}
+          <select
+            onChange={({ target }) => {
+              document.documentElement.className = target.value;
+              window.localStorage.setItem('theme', target.value);
+            }}
+            defaultValue={window.localStorage.getItem('theme') || Themes.OSDefault}
+          >
+            <option value={Themes.OSDefault}>OS default</option>
+            <option value={Themes.Light}>Light</option>
+            <option value={Themes.Dark}>Dark</option>
+          </select>
+        </div>
+        <button onClick={onSignOutClick}>Log Out</button>
+      </div>
+    </div>
+  );
+};
