7a86a18c8b371142c41d9cb1a56e509ad67038bf
Nicholas Victoros <nvictoros21@gmail.com>
2025-01-06 20:23:00 +0000

    Sync marquee speeds

--- src/app/ui/components/Marquee.tsx
+++ src/app/ui/components/Marquee.tsx
@@ -10,16 +10,48 @@ type MarqueeProps = {
 export const Marquee = ({ children, className, space = 8 }: MarqueeProps) => {
   const marqueeWrapper = useRef<HTMLDivElement | null>(null);
   const marqueeContent = useRef<HTMLDivElement | null>(null);
+  const marqueeTextElement = useRef<HTMLDivElement | null>(null);
+  const marqueeContentWithSpaces = useRef<HTMLDivElement | null>(null);
   const [marqueeText, setMarqueeText] = useState<boolean>(false);
 
   useEffect(() => {
-    const marqueeContentWidth = marqueeContent?.current?.scrollWidth || 0;
+    const marqueeContentWidth = marqueeContent?.current?.clientWidth || 0;
     const marqueeWrapperWidth = marqueeWrapper?.current?.clientWidth || 0;
 
     if (marqueeContentWidth > marqueeWrapperWidth) {
       setMarqueeText(true);
     }
   }, [children]);
+
+  useEffect(() => {
+    const animationMsPerPx = 40;
+    const duration = (marqueeContentWithSpaces?.current?.getBoundingClientRect().width || 0) * animationMsPerPx;
+    const initialDelay = 1000;
+    const delayOffset = initialDelay / duration;
+
+    if (marqueeText) {
+      marqueeTextElement?.current?.animate(
+        [
+          {
+            transform: 'translate(0, 0)',
+            offset: 0,
+          },
+          {
+            transform: 'translate(0, 0)',
+            offset: delayOffset,
+          },
+          {
+            transform: 'translate(-50%, 0)',
+            offset: 1,
+          },
+        ],
+        {
+          duration,
+          iterations: Infinity,
+        },
+      );
+    }
+  }, [marqueeText]);
 
   return (
     <span ref={marqueeWrapper}>
@@ -28,8 +60,8 @@ export const Marquee = ({ children, className, space = 8 }: MarqueeProps) => {
       </p>
       {marqueeText ? (
         <div className={styles.wrapper}>
-          <div className={styles.marquee}>
-            <p className={className}>
+          <div ref={marqueeTextElement} className={styles.marquee}>
+            <p ref={marqueeContentWithSpaces} className={className}>
               {children}
               {'\u00A0'.repeat(space)}
             </p>
